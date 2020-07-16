cab5a57bafac97b7dccc7f9756bc7e41d9e50b70
Hazem AbdelMagid <hazemahmedabdelmagid@gmail.com>
2020-07-16 21:04:32 +0200

    My Final Project

--- ansible-demos-exercises/Final-Project/Flush-Priv.sh
+++ ansible-demos-exercises/Final-Project/Flush-Priv.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-mysql -u ecomuser -pecompassword <<EOF
+#mysql -u root -proot <<EOF
+#SELECT user FROM mysql.user;
+#EOF
+
+#x=`grep 'ecomdb' | wc -l`
+
+#if [[ $x -ne 1 ]]
+#then
+
+mysql -u root -proot <<EOF
+GRANT ALL ON ecomdb.* TO 'ecomuser'@'192.168.1.29' IDENTIFIED BY 'ecompassword' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
+
+#fi
--- ansible-demos-exercises/Final-Project/MySql-Script.sh
+++ ansible-demos-exercises/Final-Project/MySql-Script.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#mysql -u ecomuser -pecompassword ecomdb <<EOF
+#SHOW TABLES;
+#EOF
+
+#x=`grep 'products' | wc -l`
+
+#if [[ $x -ne 1 ]]
+#then
+
 mysql -u ecomuser -pecompassword ecomdb < /tmp/db-load-script.sql
+
+#fi
